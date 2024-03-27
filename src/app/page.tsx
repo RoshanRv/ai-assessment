9216a37e0768b5da3727992946f111915bb371f6
RoshanRv <nkroshankumar@gmail.com>
2024-03-27 06:13:20 +0530

    setUser added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,18 @@ import HomeContent from "@/components/HomeContent";
 import Image from "next/image";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import useUser from "@/store/useUser";
 
 export default function Home() {
   const router = useRouter();
+  const setUser = useUser((state) => state.setUser);
+  const setRole = useUser((state) => state.setRole);
+
   useEffect(() => {
     const details = localStorage.getItem("ai-assessment");
     if (details) {
-      router.push("/assessment");
+      setUser(JSON.parse(details));
+      setRole(JSON.parse(details).role);
     }
   }, []);
 
