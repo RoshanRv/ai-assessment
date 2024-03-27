4f4547896318d8ab631e5e469a07a1d3ed90da02
SivaPrakash8825 <sivaprakash2122003@gmail.com>
2024-03-27 06:08:40 +0530

    small change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,12 +3,11 @@
 import Button from "@/components/button/Button";
 import axios from "axios";
 import React, { useState } from "react";
-import { FaUserCircle } from "react-icons/fa";
-import { RiLockPasswordFill } from "react-icons/ri";
-
+import { useRouter } from "next/navigation";
 type Props = {};
 
 const page = (props: Props) => {
+  // const router = useT
   const [userName, setUserName] = useState("");
   const [pass, setPass] = useState("");
   const Login = async () => {
@@ -20,39 +19,42 @@ const page = (props: Props) => {
           pass: pass,
         }
       );
-      localStorage.setItem("ai-assessment", JSON.stringify(data));
+      if (data) {
+        localStorage.setItem("ai-assessment", JSON.stringify(data));
+      }
     } else {
       alert("Fill the details");
     }
   };
 
   return (
-    <div className=" w-full h-[100vh] flex justify-center items-center bg">
+    <div className=" w-full h-[100vh] flex justify-center items-center">
+      {/* BG Designs */}
+      <div className="w-[30rem] absolute top-[30%] left-[80%]  rotate-45  h-[50rem] bg-gradient-to-br from-priClr/60 to-secClr/60 rounded-tr-2xl  blur-3xl " />
+      <div className="w-[30rem] absolute bottom-[30%] right-[80%]  rotate-45  h-[50rem] bg-gradient-to-br from-priClr/60 to-secClr/60 rounded-tr-2xl  blur-3xl " />
       {/* --- BG Designs --- */}
-      <div className=" w-96 h-auto py-10 text-center px-7 p-5 CardShadow flex flex-col gap-y-6  border-[3px] bg-white z-10 border-priClr  ">
-        <h1 className=" font-black tracking-wider text-3xl">LOGIN </h1>
-        <div className=" flex items-center h-12 w-full border-black border boxShadow">
-          <div
+      <div className=" w-96 h-auto py-10 text-center px-7 p-5 CardShadow flex flex-col gap-y-4  border-[3px] bg-white z-10 border-priClr  ">
+        <h1 className=" font-black tracking-wider text-3xl">STAFF LOGIN </h1>
+        <div className=" flex items-center h-12 w-full border-black border">
+          {/* <div
             className="flex justify-center items-center 
-           text-black w-14 h-full "
-          >
-            <FaUserCircle className=" text-priClr text-2xl" />
-          </div>
+           text-black w-14 h-full ">
+            <FaUserCircle className="  text-2xl" />
+          </div> */}
           <input
             type="text"
-            placeholder="Email"
+            placeholder="Staff Name or Email"
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
             className="px-2 h-full outline-none w-full boxShadow"
           />
         </div>
-        <div className=" flex items-center h-12 w-full boxShadow border-black border">
-          <div
+        <div className=" flex items-center h-12 w-full border-black border">
+          {/* <div
             className="flex justify-center items-center 
-           text-black w-14 h-full "
-          >
-            <RiLockPasswordFill className=" text-priClr text-2xl" />
-          </div>
+           text-black w-14 h-full ">
+            <RiLockPasswordFill className="  text-2xl" />
+          </div> */}
           <input
             type="text"
             placeholder="Password"
