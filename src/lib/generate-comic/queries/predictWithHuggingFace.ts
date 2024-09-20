0d44248780e902b338e7d3be991fb358a2b16c43
SivaPrakash8825 <sivaprakash2122003@gmail.com>
2024-09-21 03:48:29 +0530

    Refactor predict function to use GoogleGenerativeAI for text generation

--- src/lib/generate-comic/queries/predictWithHuggingFace.ts
+++ src/lib/generate-comic/queries/predictWithHuggingFace.ts
@@ -1,67 +1,31 @@
 "use server";
-
-import { HfInference, HfInferenceEndpoint } from "@huggingface/inference";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export async function predict(
   inputs: string,
   nbMaxNewTokens: number
 ): Promise<string> {
-  const hf = new HfInference(process.env.AUTH_HF_API_TOKEN);
+  const genAI = new GoogleGenerativeAI(
+    "AIzaSyApzireBTzNrki4j_ycbDForcnTNslCQiM"
+  );
 
-  const llmEngine = `${process.env.LLM_ENGINE || ""}` as LLMEngine;
-  const inferenceEndpoint = `${
-    process.env.LLM_HF_INFERENCE_ENDPOINT_URL || ""
-  }`;
-  const inferenceModel = `${process.env.LLM_HF_INFERENCE_API_MODEL || ""}`;
-
-  let hfie: HfInferenceEndpoint = hf;
-
-  switch (llmEngine) {
-    case "INFERENCE_ENDPOINT":
-      if (inferenceEndpoint) {
-        //  console.log("Using a custom HF Inference Endpoint")
-        hfie = hf.endpoint(inferenceEndpoint);
-      } else {
-        const error = "No Inference Endpoint URL defined";
-        console.error(error);
-        throw new Error(error);
-      }
-      break;
-
-    case "INFERENCE_API":
-      if (inferenceModel) {
-        // console.log("Using an HF Inference API Model")
-      } else {
-        const error = "No Inference API model defined";
-        console.error(error);
-        throw new Error(error);
-      }
-      break;
-
-    default:
-      const error =
-        "Please check your Hugging Face Inference API or Inference Endpoint settings";
-      console.error(error);
-      throw new Error(error);
-  }
-
-  const api = llmEngine === "INFERENCE_ENDPOINT" ? hfie : hf;
-
+  const model = genAI.getGenerativeModel({
+    model: "gemini-pro",
+    generationConfig: {
+      maxOutputTokens: nbMaxNewTokens,
+    },
+  });
   let instructions = "";
+
   try {
-    for await (const output of api.textGenerationStream({
-      model:
-        llmEngine === "INFERENCE_ENDPOINT"
-          ? undefined
-          : inferenceModel || undefined,
-      inputs,
-      parameters: {
-        do_sample: true,
-        max_new_tokens: nbMaxNewTokens,
-        return_full_text: false,
-      },
-    })) {
-      instructions += output.token.text;
+    for await (const output of (await model.generateContentStream(inputs))
+      .stream) {
+      if (output.candidates) {
+        console.log(output.candidates[0].content);
+
+        instructions += output.candidates[0].content.parts[0].text;
+      }
+
       // process.stdout.write(output.token.text)
       if (
         instructions.includes("</s>") ||
